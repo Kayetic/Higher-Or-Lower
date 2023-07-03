8f35c54cd2b4e107aa9237af66afa4b64479d4bf
Kayetic <bartek.bak@protonmail.com>
2023-07-03 22:48:41 +0100

    finished mvp, no api yet and only 'hotter' text

---
+++ index.js
@@ -0,0 +1,6 @@
+const higherLowerText = document.querySelector(".higher-lower-text");
+
+setTimeout(function () {
+  higherLowerText.style.letterSpacing = "4px";
+  higherLowerText.style.fontSize = "4rem";
+}, 100);
