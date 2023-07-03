afa26677e48440d9da65b7fb1c861887f811751a
Kayetic <bartek.bak@protonmail.com>
2023-07-03 23:12:10 +0100

    fixed the letter spacing adding extra margin to the side of the main line and adjusted overall sizing

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const higherLowerText = document.querySelector(".higher-lower-text");
 
 setTimeout(function () {
   higherLowerText.style.letterSpacing = "4px";
-  higherLowerText.style.fontSize = "4rem";
+  higherLowerText.style.fontSize = "5rem";
+  higherLowerText.style.marginRight = "-4px"; // add this line
 }, 100);
