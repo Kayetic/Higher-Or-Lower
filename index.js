c6340e5601398a8deea133d7794c0578cade9350
Kayetic <bartek.bak@protonmail.com>
2023-07-03 23:17:17 +0100

    attempt to fix scrollbar showing on mobile, even with height: 100vh

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const higherLowerText = document.querySelector(".higher-lower-text");
 
+// Calculate the viewport height and represent it as CSS custom property
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+// Recalculate on resize
+window.addEventListener("resize", () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
+
 setTimeout(function () {
   higherLowerText.style.letterSpacing = "4px";
-  higherLowerText.style.fontSize = "5rem";
-  higherLowerText.style.marginRight = "-4px"; // add this line
+  higherLowerText.style.fontSize = "4rem";
 }, 100);
