df44e483fe12113acacbebca4885129eb8a07b46
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2023-07-04 11:19:39 +0000

    making api calls to openweathermap

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const higherLowerText = document.querySelector(".higher-lower-text");
-
-// Calculate the viewport height and represent it as CSS custom property
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty("--vh", `${vh}px`);
-
-// Recalculate on resize
-window.addEventListener("resize", () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-});
+const weatherAPIKey = "";
+const londonLatitute = "51.5074";
+const londonLongitude = "0.1278";
 
 setTimeout(function () {
   higherLowerText.style.letterSpacing = "4px";
   higherLowerText.style.fontSize = "4rem";
 }, 100);
+
+console.log(weatherAPIKey);
+
+const getCurrentWeather = async function () {
+  let url = `https://api.openweathermap.org/data/3.0/onecall?lat={${londonLatitute}}&lon={${londonLongitude}}&exclude={}&appid={${weatherAPIKey}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+};
+
+getCurrentWeather();
