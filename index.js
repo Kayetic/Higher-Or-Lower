9b367957b34c1d97edd9e5972a2542a19b26b8dd
Kayetic <bartek.bak@protonmail.com>
2023-07-07 15:33:27 +0100

    fixed api call permissions, trying to average temps from 11am, 12am, 1pm and 2pm

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const higherLowerText = document.querySelector(".higher-lower-text");
 const weatherAPIKey = "";
-const londonLatitute = "51.5074";
-const londonLongitude = "0.1278";
+const londonLatitute = "51.51";
+const londonLongitude = "0.13";
 
 setTimeout(function () {
   higherLowerText.style.letterSpacing = "4px";
@@ -11,10 +11,21 @@ setTimeout(function () {
 console.log(weatherAPIKey);
 
 const getCurrentWeather = async function () {
-  let url = `https://api.openweathermap.org/data/3.0/onecall?lat={${londonLatitute}}&lon={${londonLongitude}}&exclude={}&appid={${weatherAPIKey}`;
+  let url =
+    "https://api.openweathermap.org/data/3.0/onecall?lat=51.51&lon=0.13&exclude=current,minutely,hourly,alerts&appid=42d397e5a775ae63f3b94f0e308f7fc3";
   const response = await fetch(url);
   const data = await response.json();
   console.log(data);
+  // getting just the hourly forecast of temperatures into an array
+  //   const hourlyForecast = data.daily[0].temp;
+  // get the temperatures at 11am, 12am, 1pm and 2pm then take the average
+  const averageTemp =
+    (hourlyForecast.morn +
+      hourlyForecast.day +
+      hourlyForecast.eve +
+      hourlyForecast.night) /
+    4;
+  //   console.log(hourlyForecast);
 };
 
 getCurrentWeather();
